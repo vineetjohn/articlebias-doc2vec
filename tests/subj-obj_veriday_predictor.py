d0b05ec95c05e7eee880bd9c86102f14466a0c4f
Vineet <vinjohn10@gmail.com>
2016-12-19 18:10:54 -0500

    Modified unit-test to support custom Subjective vs Objective classifier

--- tests/subj-obj_veriday_predictor.py
+++ tests/subj-obj_veriday_predictor.py
@@ -1,8 +1,12 @@
 import json
+import pickle
+import numpy as np
 
 from gensim.models.doc2vec import Doc2Vec
 from utils import scikit_ml_helper
 from utils import log_helper
+
+from sklearn import metrics
 
 doc2vec_model_path = \
     "/home/v2john/Dropbox/Personal/Academic/Masters/UWaterloo/Academics/" + \
@@ -18,7 +22,10 @@ veriday_articles_path = \
 
 veriday_predicted_articles_path = \
     "/home/v2john/Dropbox/Personal/Academic/Masters/UWaterloo/Academics/" + \
-    "ResearchProject/Veriday/annotated/all_articles_predicted.json"
+    "ResearchProject/Veriday/annotated/all_articles_predicted_new.json"
+
+avg_x_obj_path = "/home/v2john/avg_x_obj"
+avg_x_sub_path = "/home/v2john/avg_x_sub"
 
 log = log_helper.get_logger("VeridayPredict2Class")
 
@@ -31,6 +38,12 @@ def load_models():
 log.info("Begun execution")
 doc2vec_model, ml_model = load_models()
 log.info("Models loaded")
+
+with open(avg_x_obj_path) as avg_x_obj_file:
+    avg_x_obj = pickle.load(avg_x_obj_file)
+with open(avg_x_sub_path) as avg_x_sub_file:
+    avg_x_sub = pickle.load(avg_x_sub_file)
+log.info("Avg vectors unpickled")
 
 with open(veriday_articles_path) as veriday_articles_file:
     veriday_articles = json.load(veriday_articles_file)
@@ -49,25 +62,36 @@ for veriday_article in veriday_articles:
         predicted_veriday_article['articleid'] = veriday_article['articleid'].strip()
         predicted_veriday_article['value'] = veriday_article['value'].strip()
 
-        x_vectors.append(doc2vec_model.infer_vector(predicted_veriday_article['value']))
+        x_vector = doc2vec_model.infer_vector(predicted_veriday_article['value'])
+        obj_sim = metrics.pairwise.cosine_similarity(np.array(x_vector).reshape(1, -1), np.array(avg_x_obj).reshape(1, -1))
+        sub_sim = metrics.pairwise.cosine_similarity(np.array(x_vector).reshape(1, -1), np.array(avg_x_sub).reshape(1, -1))
+        log.info("obj_dist: " + str(obj_sim[0][0]))
+        log.info("sub_dist: " + str(sub_sim[0][0]))
+
+        if obj_sim >= sub_sim:
+            predicted_veriday_article['prediction'] = "Objective"
+        else:
+            predicted_veriday_article['prediction'] = "Subjective"
+
+        x_vectors.append(x_vector)
 
         predicted_veriday_articles.append(predicted_veriday_article)
         counter += 1
 
 log.info("Veriday article have been vectorized")
 
-y_pred = ml_model.predict(x_vectors)
-log.info("Predictions have been made")
-
-for i in xrange(len(predicted_veriday_articles)):
-    if y_pred[i] == 0:
-        predicted_veriday_articles[i]['prediction'] = "objective"
-    elif y_pred[i] == 1:
-        predicted_veriday_articles[i]['prediction'] = "subjective"
-    else:
-        log.error("Invalid prediction made for article " + \
-                  predicted_veriday_articles[i]['articleid'] + \
-                  ": " + str(y_pred))
+# y_pred = ml_model.predict(x_vectors)
+# log.info("Predictions have been made")
+#
+# for i in xrange(len(predicted_veriday_articles)):
+#     if y_pred[i] == 0:
+#         predicted_veriday_articles[i]['prediction'] = "objective"
+#     elif y_pred[i] == 1:
+#         predicted_veriday_articles[i]['prediction'] = "subjective"
+#     else:
+#         log.error("Invalid prediction made for article " + \
+#                   predicted_veriday_articles[i]['articleid'] + \
+#                   ": " + str(y_pred))
 log.info("Articles have been annotated with predictions")
 
 with open(veriday_predicted_articles_path, 'w') as veriday_predicted_articles_file:
